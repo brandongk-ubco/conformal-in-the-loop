6fdb42e529813f6c5bd881502f58dc2eca82fa24
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2023-11-21 18:46:40 +0000

    Updating uncertainty stuff.

--- confidentaugmentation/model.py
+++ confidentaugmentation/model.py
@@ -5,6 +5,8 @@ import torch.nn.functional as F
 from mapie.classification import MapieClassifier
 from pytorch_lightning.utilities.types import EVAL_DATALOADERS
 from torch import nn
+from torchmetrics.aggregation import MeanMetric
+from torchmetrics.classification.accuracy import Accuracy
 
 
 class Net(nn.Module):
@@ -54,12 +56,16 @@ class Net(nn.Module):
 
 
 class ConformalTrainer(L.LightningModule):
-    def __init__(self, model):
+    def __init__(self, model, selectively_backpropagate=False):
         super().__init__()
-        self.save_hyperparameters(ignore=["model", "calib_set"])
+        self.save_hyperparameters(ignore=["model"])
         self.model = model
 
         self.classes_ = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        self.accuracy =  Accuracy(task="multiclass", num_classes=len(self.classes_))
+
+        self.selectively_backpropagate = selectively_backpropagate
 
     def __sklearn_is_fitted__(self):
         return True
@@ -76,24 +82,29 @@ class ConformalTrainer(L.LightningModule):
     def forward(self, x):
         if x.dim() == 2:
             return (
-                self.model(x.unsqueeze(0).unsqueeze(0)).softmax(1).squeeze(0).squeeze(0)
+                self.model(x.unsqueeze(0).unsqueeze(0))
             )
         elif x.dim() == 3:
-            return self.model(x.unsqueeze(0)).softmax(1).squeeze(0)
+            return self.model(x.unsqueeze(0))
         elif x.dim() == 4:
-            return self.model(x).softmax(1)
+            return self.model(x)
         else:
             import pdb
 
             pdb.set_trace()
-            raise ValueError("Input must be 3 or 4 dimensional")
+            raise ValueError("Input must be 2, 3 or 4 dimensional")
+
+    def on_train_epoch_start(self) -> None:
+        self.uncertain_percentage = MeanMetric().to(self.device)
+        self.confused_percentage = MeanMetric().to(self.device)
+        self.realized_percentage = MeanMetric().to(self.device)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
 
         self.cp_examples = list(
-            zip(y_hat.detach().cpu().numpy(), y.detach().cpu().numpy())
+            zip(y_hat.detach().softmax(axis=1).cpu().numpy(), y.detach().cpu().numpy())
         )
 
         num_classes = (
@@ -102,9 +113,37 @@ class ConformalTrainer(L.LightningModule):
             .squeeze()
         )
 
-        loss = F.cross_entropy(y_hat, y)
+        predicted = y_hat.argmax(axis=1)
+        correct = predicted == y
 
-        self.log("loss", loss)
+        realized = torch.logical_and(correct, torch.tensor(num_classes == 1).to(device=self.device))
+        confused = torch.logical_and(~correct, torch.tensor(num_classes == 1).to(device=self.device))
+        uncertain = torch.tensor(num_classes != 1).to(device=self.device)
+
+        realized_percentage = realized.sum() / len(realized)
+        confused_percentage = confused.sum() / len(confused)
+        uncertain_percentage = uncertain.sum() / len(uncertain)
+
+        self.realized_percentage(realized_percentage)
+        self.confused_percentage(confused_percentage)
+        self.uncertain_percentage(uncertain_percentage)
+
+        torch.testing.assert_close(realized_percentage + confused_percentage + uncertain_percentage, torch.tensor(1.0).to(self.device))
+
+        metrics = {
+            "uncertain": self.uncertain_percentage.compute(),
+            "confused": self.confused_percentage.compute(),
+            "realized": self.realized_percentage.compute(),
+        }
+
+        self.log_dict(metrics, on_step=True, on_epoch=False, prog_bar=True, logger=True)
+
+
+        if self.selectively_backpropagate:
+            loss = F.cross_entropy(y_hat, y, reduction='none')[uncertain].mean()
+        else:
+            loss = F.cross_entropy(y_hat, y)
+
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -113,18 +152,19 @@ class ConformalTrainer(L.LightningModule):
         test_loss = F.cross_entropy(y_hat, y)
 
         self.cp_examples += list(
-            zip(y_hat.detach().cpu().numpy(), y.detach().cpu().numpy())
+            zip(y_hat.detach().softmax(axis=1).cpu().numpy(), y.detach().cpu().numpy())
         )
 
-        self.log("test_loss", test_loss)
+        self.accuracy(y_hat, y)
+
+        self.log('accuracy', self.accuracy, on_step=False, on_epoch=True)
+
+        self.log("val_loss", test_loss, on_step=False, on_epoch=True)
 
     def on_validation_epoch_start(self) -> None:
-        super().on_validation_epoch_start()
         self.cp_examples = []
 
     def on_validation_epoch_end(self) -> None:
-        super().on_validation_epoch_end()
-
         self.mapie_classifier = MapieClassifier(
             estimator=self, method="score", cv="prefit"
         ).fit(np.array(range(len(self.cp_examples))), [v[1] for v in self.cp_examples])
