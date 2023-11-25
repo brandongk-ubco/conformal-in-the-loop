ce8ccf386eb1eeed41dec43efec2a0a61299d4d3
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2023-11-25 04:51:04 +0000

    More options in loop.

--- train.sh
+++ train.sh
@@ -8,11 +8,14 @@ for augmentation_policy in noop cifar10; do
     for model_name in efficientnet-b0 efficientnet-b3; do
         for selectively_backpropagate in "--no-selectively-backpropagate" "--selectively-backpropagate"; do
             for mapie_alpha in 0.10 0.05 0.03; do
-                python -m confidentaugmentation cifar10 \
-                    "--model-name=${model_name}" \
-                    "--augmentation-policy-path=./policies/${augmentation_policy}.yaml" \
-                    "${selectively_backpropagate}" \
-                    --mapie-alpha "${mapie_alpha}"
+                for pretrained in "--pretrained" "--no-pretrained"; do
+                    python -m confidentaugmentation cifar10 \
+                        "--model-name=${model_name}" \
+                        "--augmentation-policy-path=./policies/${augmentation_policy}.yaml" \
+                        "${pretrained}" \
+                        "${selectively_backpropagate}" \
+                        "--mapie-alpha=${mapie_alpha}"
+                done
             done
         done
     done
