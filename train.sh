d52e248c4bf42b0ceba1d883c4c6211ff8d0bc88
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2023-12-05 05:00:46 +0000

    More tests.

--- train.sh
+++ train.sh
@@ -5,13 +5,13 @@ set -eux
 rm -rf lightning_logs
 
 for augmentation_policy in "cifar10"; do
-    for model_name in "efficientnet_b0"; do
+    for model_name in "efficientnet_b0" "efficientnet_b1" "efficientnet_b3" "efficientnet_b5"; do
         for mapie_alpha in 0.10; do
-            for selectively_backpropagate in "--selectively-backpropagate" "--no-selectively-backpropagate"; do
+            for selectively_backpropagate in "--selectively-backpropagate"; do
                 for pretrained in "--pretrained"; do
                     for use_pid in "--use-pid" "--no-use-pid"; do
                         for lr_method in "plateau" "uncertainty"; do
-                            for optimizer in "SGD"; do
+                            for optimizer in "SGD" "Adam"; do
                                 python -m confidentaugmentation train cifar10 \
                                     "--model-name=${model_name}" \
                                     "--augmentation-policy-path=./policies/${augmentation_policy}.yaml" \
@@ -19,7 +19,8 @@ for augmentation_policy in "cifar10"; do
                                     "${selectively_backpropagate}" \
                                     "${use_pid}" \
                                     "--mapie-alpha=${mapie_alpha}" \
-                                    "--lr-method=${lr_method}"
+                                    "--lr-method=${lr_method}" \
+                                    "--optimizer=${optimizer}"
                             done
                         done
                     done
