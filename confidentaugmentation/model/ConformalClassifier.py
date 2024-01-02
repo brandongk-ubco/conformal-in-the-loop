f5d936a3bd1e027dc240a93a1845abbd94a6888d
Brandon Graham-Knight <brandongk@alumni.ubc.ca>
2024-01-02 04:56:58 +0000

    More clean-up

--- confidentaugmentation/model/ConformalClassifier.py
+++ confidentaugmentation/model/ConformalClassifier.py
@@ -31,11 +31,6 @@ class ConformalClassifier(L.LightningModule):
 
         self.accuracy = Accuracy(task="multiclass", num_classes=len(self.classes_))
 
-        self.atypical_percentage = MeanMetric()
-        self.uncertain_percentage = MeanMetric()
-        self.confused_percentage = MeanMetric()
-        self.realized_percentage = MeanMetric()
-
         self.selectively_backpropagate = selectively_backpropagate
         self.mapie_alpha = mapie_alpha
         self.val_mapie_alpha = val_mapie_alpha
@@ -74,13 +69,6 @@ class ConformalClassifier(L.LightningModule):
             y_hat, _ = y_hat
 
         return y_hat
-
-    def on_train_epoch_start(self) -> None:
-        super().on_train_epoch_start()
-        self.atypical_percentage.reset()
-        self.uncertain_percentage.reset()
-        self.confused_percentage.reset()
-        self.realized_percentage.reset()
 
     def training_step(self, batch, batch_idx):
         x, y, indeces = batch
@@ -133,11 +121,6 @@ class ConformalClassifier(L.LightningModule):
         confused_percentage = confused.sum() / len(confused)
         uncertain_percentage = uncertain.sum() / len(uncertain)
 
-        self.atypical_percentage(atypical_percentage)
-        self.realized_percentage(realized_percentage)
-        self.confused_percentage(confused_percentage)
-        self.uncertain_percentage(uncertain_percentage)
-
         torch.testing.assert_close(
             atypical_percentage
             + realized_percentage
@@ -147,10 +130,10 @@ class ConformalClassifier(L.LightningModule):
         )
 
         metrics = {
-            "atypical": self.atypical_percentage.compute(),
-            "uncertain": self.uncertain_percentage.compute(),
-            "confused": self.confused_percentage.compute(),
-            "realized": self.realized_percentage.compute(),
+            "atypical": atypical_percentage,
+            "uncertain": uncertain_percentage,
+            "confused": confused_percentage,
+            "realized": realized_percentage,
         }
         self.log_dict(metrics, on_step=True, on_epoch=False, prog_bar=True, logger=True)
 
@@ -260,20 +243,29 @@ class ConformalClassifier(L.LightningModule):
         y_hat = self(x)
         test_loss = F.cross_entropy(y_hat, y)
 
-        conformal_sets = self.mapie_classifier.predict(
-            range(len(self.cp_examples)), alpha=[self.val_mapie_alpha]
-        )[1]
+        self.cp_examples = list(
+            zip(y_hat.detach().softmax(axis=1).cpu().numpy(), y.detach().cpu().numpy())
+        )
 
-        num_classes = conformal_sets.sum(axis=1).squeeze()
+        num_classes = (
+            self.mapie_classifier.predict(
+                range(len(self.cp_examples)), alpha=[self.mapie_alpha]
+            )[1]
+            .sum(axis=1)
+            .squeeze()
+        )
 
-        conformal_predictions = conformal_sets.argmax(axis=1).squeeze()
+        predicted = y_hat.argmax(axis=1)
+        correct = predicted == y
 
-        correct = conformal_predictions == self.val_labels
-
-        atypical = num_classes == 0
-        realized = np.logical_and(correct, num_classes == 1)
-        confused = np.logical_and(~correct, num_classes == 1)
-        uncertain = num_classes > 1
+        atypical = torch.tensor(num_classes == 0).to(device=self.device)
+        realized = torch.logical_and(
+            correct, torch.tensor(num_classes == 1).to(device=self.device)
+        )
+        confused = torch.logical_and(
+            ~correct, torch.tensor(num_classes == 1).to(device=self.device)
+        )
+        uncertain = torch.tensor(num_classes > 1).to(device=self.device)
 
         atypical_percentage = atypical.sum() / len(atypical)
         realized_percentage = realized.sum() / len(realized)
