d07b39af1de694d76279934d1b1859897f88c123
John Jones <brandongk@alumni.ubc.ca>
2024-04-12 18:00:46 +0000

    Simplify your life.

--- test/conformal_classifier_test.py
+++ test/conformal_classifier_test.py
@@ -36,13 +36,48 @@ class TestConformalClassifier:
 
         cc.reset()
         cc.append(y_hat, y)
-        results = cc.measure_uncertainty()
-        assert len(results["conformal_sets"]) == num_examples
-        assert len(results["num_classes"]) == num_examples
+        conformal_sets, results = cc.measure_uncertainty()
+        assert len(conformal_sets) == num_examples
+        assert len(results["prediction_set_size"]) == num_examples
         assert len(results["atypical"]) == num_examples
         assert len(results["realized"]) == num_examples
         assert len(results["confused"]) == num_examples
         assert len(results["uncertain"]) == num_examples
+
+    def test_predict_percentage(self):
+        num_examples = 256
+        num_classes = 10
+        y_hat = torch.rand(num_examples, num_classes).softmax(axis=1).numpy()
+        y = y_hat.argmax(axis=1)
+
+        cc = ConformalClassifier()
+        cc.append(y_hat, y)
+        cc.fit(percentage=0.5)
+        conformal_sets, results = cc.measure_uncertainty(percentage=0.5)
+        assert len(conformal_sets) == num_examples // 2
+        assert len(results["prediction_set_size"]) == num_examples // 2
+        assert len(results["atypical"]) == num_examples // 2
+        assert len(results["realized"]) == num_examples // 2
+        assert len(results["confused"]) == num_examples // 2
+        assert len(results["uncertain"]) == num_examples // 2
+
+    def test_predict_uneven_percentage(self):
+        num_examples = 256
+        num_classes = 10
+        y_hat = torch.rand(num_examples, num_classes).softmax(axis=1).numpy()
+        y = y_hat.argmax(axis=1)
+
+        cc = ConformalClassifier()
+        cc.append(y_hat, y)
+        cc.fit(percentage=0.2)
+        conformal_sets, results = cc.measure_uncertainty(percentage=0.8)
+        assert len(conformal_sets) == int(num_examples * 0.8)
+        assert len(results["prediction_set_size"]) == int(num_examples * 0.8)
+        assert len(results["atypical"]) == int(num_examples * 0.8)
+        assert len(results["realized"]) == int(num_examples * 0.8)
+        assert len(results["confused"]) == int(num_examples * 0.8)
+        assert len(results["uncertain"]) == int(num_examples * 0.8)
+
 
     def test_accumulate_predict(self):
         num_examples = 256
@@ -64,9 +99,9 @@ class TestConformalClassifier:
         y = y_hat.argmax(axis=1)
         cc.append(y_hat, y)
 
-        results = cc.measure_uncertainty()
-        assert len(results["conformal_sets"]) == 2 * num_examples
-        assert len(results["num_classes"]) == 2 * num_examples
+        conformal_sets, results = cc.measure_uncertainty()
+        assert len(conformal_sets) == 2 * num_examples
+        assert len(results["prediction_set_size"]) == 2 * num_examples
         assert len(results["atypical"]) == 2 * num_examples
         assert len(results["realized"]) == 2 * num_examples
         assert len(results["confused"]) == 2 * num_examples
