9a675bf9e5838505b204b3db44fc1ea5f2355266
John Jones <brandongk@alumni.ubc.ca>
2024-08-07 22:12:09 +0000

    Tuner.

--- citl/train.py
+++ citl/train.py
@@ -153,9 +153,9 @@ def train(
         log_every_n_steps=10,
     )
 
-    # tuner = Tuner(trainer)
-    # tuner.scale_batch_size(model, datamodule=datamodule, max_trials=7)
-    # tuner.lr_find(model, datamodule=datamodule, max_lr=1e-2)
+    tuner = Tuner(trainer)
+    tuner.scale_batch_size(model, datamodule=datamodule, max_trials=7)
+    tuner.lr_find(model, datamodule=datamodule, max_lr=1e-2)
 
     trainer.fit(model=model, datamodule=datamodule)
     trainer.test(ckpt_path="best", datamodule=datamodule)
