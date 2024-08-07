efd26304859046600cf6b52c7251a4789ee3c4b7
John Jones <brandongk@alumni.ubc.ca>
2024-08-07 22:12:00 +0000

    Max Size.

--- citl/train.py
+++ citl/train.py
@@ -147,7 +147,7 @@ def train(
     trainer = L.Trainer(
         logger=trainer_logger,
         num_sanity_val_steps=sys.maxsize,
-        max_epochs=1,
+        max_epochs=sys.maxsize,
         deterministic=True,
         callbacks=callbacks,
         log_every_n_steps=10,
