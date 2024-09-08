779ab88bce224aa1fb67e0c09e6158917ca29aaa
John Jones <brandongk@alumni.ubc.ca>
2024-09-08 14:12:21 +0000

    Stratified accuracy.

---
+++ stratified_accuracy.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pandas as pd
+import json
+
+test_results = pd.read_csv('test_results.csv')
+
+test_results["ground_truth"] = test_results["label"]
+test_results["prediction"] = test_results.apply(lambda row: np.argmax(json.loads(row["probability"])), axis=1)
+del test_results["probability"]
+
+test_results['label'] = test_results['label'].replace({0: 'Not Wavy', 1: 'Wavy'})
+test_results['attribute'] = test_results['attribute'].replace({0: 'Female', 1: 'Male'})
+
+accuracy = test_results.groupby(['label', 'attribute']).apply(
+    lambda group: (group['prediction'] == group['ground_truth']).mean()
+)
+
+print(accuracy)
